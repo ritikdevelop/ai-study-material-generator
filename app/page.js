e3fc6e78f96e6c6e9b2ef1799a8de6c0fe160294
ritikdevelop <hrithikrana216@gmail.com>
2024-12-21 23:14:27 +0530

    Refactor Home component layout for improved readability

--- app/page.js
+++ app/page.js
@@ -3,12 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <div className="flex justify-center items-center h-screen">
-      <h2>
-        Hello, world!
-      </h2>
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <h2>Hello, world!</h2>
+      </div>
     </div>
-   </div>
   );
 }
